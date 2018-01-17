acb31698d052281acfc4f78c5f9e0249ea4d9179
Jerome Toulouse <monsieur.toulouse@gmail.com>
2018-01-17 16:08:18 +0100

    Add the configuration for the app in Sinatra.

--- aFlightTrackApp.rb
+++ aFlightTrackApp.rb
@@ -1,9 +1,16 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'sinatra/content_for'
 require 'tilt/erubis'
 require 'FileUtils'
 
 class FlightTrackApp < Sinatra::Application
+
+  configure do
+    enable :sessions
+    enable :secret_sessions, 'not a good secret'
+    enable :erb, escape_html: true
+  end
 
   get '/' do
     redirect '/FlightTrackApp'
