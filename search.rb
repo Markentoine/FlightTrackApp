4a91b54ada6bd1a2caba2c1785d24994eb4b77f6
Jerome Toulouse <monsieur.toulouse@gmail.com>
2018-01-19 14:17:08 +0100

    Add a Search class.

---
+++ search.rb
@@ -0,0 +1,11 @@
+class Search
+  attr_accessor :from_country, :from_town, :from_airport,
+                :to_country, :to_town, :to_airport
+
+  def initialize(from_infos, to_infos, username)
+    @from_country, @from_town, @from_airport = from_infos
+    @to_country, @to_town, @to_airport = to_infos
+    @username = username
+  end
+
+end
