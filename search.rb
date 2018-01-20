d44e04899698f04b9931a7dfd457202fbcb7ac13
Jerome Toulouse <monsieur.toulouse@gmail.com>
2018-01-20 23:02:51 +0100

    WIP.

--- search.rb
+++ search.rb
@@ -4,13 +4,4 @@ class Search
   def initialize
     @db = PG.connect(dbname: 'flights')
   end
-  # attr_accessor :from_country, :from_town, :from_airport,
-  #               :to_country, :to_town, :to_airport
-
-  # def initialize(from_infos, to_infos, username)
-  #   @from_country, @from_town, @from_airport = from_infos
-  #   @to_country, @to_town, @to_airport = to_infos
-  #   @username = username
-  # end
-
 end
